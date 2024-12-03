a5ced845eaed201c32e8ffa34849e95e1c5226cc
ChoongWengDong <weng.choong@students.fh-wels.at>
2024-12-03 10:26:48 +0100

    Update subtask1.py

--- subtask1.py
+++ subtask1.py
@@ -8,3 +8,5 @@ def largest_square(n):
 n = int(input("Enter a number: "))
 result = largest_square(n)
 print(f"The largest square less than or equal to {n} is {result}")
+
+# it looks like I learned how to use git today
